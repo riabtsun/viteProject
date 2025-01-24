74c0e0322cc603fbb8a18facf7a217f5176be8f1
Dmytro Riabtsun <riabtsun@i.ua>
2025-01-24 13:08:16 +0200

    added random bg fc to button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,10 +1,23 @@
 import classes from './button.module.css';
+import {useRef} from "react";
 
 interface ButtonProps {
   text: string;
 }
+
 const Button = ({ text }: ButtonProps) => {
-  return <button className={classes.btn}>{text}</button>;
+  const buttonRef = useRef(null);
+
+  function getRandomColor():void {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    buttonRef.current.style.backgroundColor = color;
+  }
+
+  return <button ref={buttonRef} onClick={getRandomColor} className={classes.btn}>{text}</button>;
 };
 
 export default Button;
