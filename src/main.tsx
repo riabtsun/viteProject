49735e364c4a167a92950e6b787b0a130c5f64c3
Dmytro Riabtsun <riabtsun.dv@gmail.com>
2025-02-06 11:42:15 +0200

    added HashRouter instead BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import './index.css';
 import App from './App.tsx';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, HashRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Router>
+    <HashRouter>
       <App />
-    </Router>
+    </HashRouter>
   </StrictMode>
 );
