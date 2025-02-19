e55eedb36d1e10b0b2f3d193457842f9f2e82e90
Dmytro Riabtsun <riabtsun.dv@gmail.com>
2025-02-19 17:10:34 +0200

    removed userContext

--- src/reducers/shopReducer.ts
+++ src/reducers/shopReducer.ts
@@ -1,21 +1,21 @@
-import { UserShopState } from '../contexts/UserContext.tsx';
-import { ProductData } from '../types/productData.ts';
-
-export interface ActionState {
-  type: string;
-  payload?: ProductData;
-}
-
-const shopReducer = (state: UserShopState, action: ActionState) => {
-  const { type, payload } = action;
-  switch (type) {
-    case 'ADD_TO_CART': {
-      console.log('Add to cart', payload);
-      return { ...state };
-    }
-    default:
-      return state;
-  }
-};
-
-export default shopReducer;
+// import { UserShopState } from '../contexts/UserContext.tsx';
+// import { ProductData } from '../types/productData.ts';
+//
+// export interface ActionState {
+//   type: string;
+//   payload?: ProductData;
+// }
+//
+// const shopReducer = (state: UserShopState, action: ActionState) => {
+//   const { type, payload } = action;
+//   switch (type) {
+//     case 'ADD_TO_CART': {
+//       console.log('Add to cart', payload);
+//       return { ...state };
+//     }
+//     default:
+//       return state;
+//   }
+// };
+//
+// export default shopReducer;
