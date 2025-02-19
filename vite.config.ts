d621032cf1a43e7a48ca025432677a5051114ec8
Dmytro Riabtsun <riabtsun.dv@gmail.com>
2025-02-19 16:49:30 +0200

    added vercel in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
+import vercel from 'vite-plugin-vercel';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    port: process.env.PORT as unknown as number,
+  },
+  plugins: [react(), vercel()],
 });
