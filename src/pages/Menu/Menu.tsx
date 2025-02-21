6640315bc94c5cd56bfb1426dc1ac832ee3db76c
Dmytro Riabtsun <riabtsun.dv@gmail.com>
2025-02-21 14:08:10 +0200

    added interface to useFetch

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,11 +1,16 @@
 // import { useEffect } from 'react';
 // import { useDispatch, useSelector } from 'react-redux';
 import '../../App.css';
+import ProductItem from '../../components/ProductItem/ProductItem.tsx';
 import useFetch from '../../hooks/useFetch.ts';
+import { ProductData } from '../../types/productData.ts';
 // import { getAllData } from '../../redux/slices/cartSlice.ts';
 // import { AppDispatch, RootState } from '../../redux/store.ts';
-import ProductItem from '../../components/ProductItem/ProductItem.tsx';
-import { ProductData } from '../../types/productData.ts';
+
+interface IProduct {
+  data: ProductData[];
+  success: string;
+}
 
 const Menu = () => {
   // const { menuItems, isLoading, error } = useSelector(
@@ -13,7 +18,7 @@ const Menu = () => {
   // );
   // const dispatch = useDispatch<AppDispatch>();
 
-  const { data, isLoading, isError } = useFetch<ProductData[]>(
+  const { data, isLoading, isError } = useFetch<IProduct>(
     'https://react-fast-pizza-api.onrender.com/api/menu'
   );
 
