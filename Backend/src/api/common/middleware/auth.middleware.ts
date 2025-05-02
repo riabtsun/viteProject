b42c5a2dd466fb2916efe49fac0a8d50e2fc3d30
Dmytro Riabtsun <riabtsun.dv@gmail.com>
2025-05-02 17:21:19 +0300

    refactoring code

--- Backend/src/api/common/middleware/auth.middleware.ts
+++ Backend/src/api/common/middleware/auth.middleware.ts
@@ -1,14 +1,24 @@
-import jwt from 'jsonwebtoken';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 import { NextFunction, Response, Request } from 'express';
 
-module.exports.checkAuth = (expectedIssuer) => {
+declare module 'express' {
+  interface Request {
+    user?: jwt.JwtPayload;
+  }
+}
+
+module.exports.checkAuth = (expectedIssuer: string) => {
   return (req: Request, res: Response, next: NextFunction) => {
     const token = req.cookies.token;
 
     if (!token) return res.status(401).json({ message: 'Unauthorized' });
 
     try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      if (!process.env.JWT_SECRET) {
+        throw new Error('JWT_SECRET is not defined');
+      }
+
+      const decoded = jwt.verify(token, process.env.JWT_SECRET) as JwtPayload;
 
       if (!decoded) return res.status(401).json({ message: 'Unauthorized' });
       if (decoded.iss || decoded.iss !== expectedIssuer) return res.status(401).json({ message: 'Unauthorized' });
